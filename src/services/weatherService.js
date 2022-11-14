9e2b0852727edcda22da096ed01f0e0ac0bca04c
ArnabDey1543 <arnabdey1543@gmail.com>
2022-11-14 21:26:38 +0530

    adding files

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,6 +1,6 @@
 import { DateTime } from "luxon";
 
-const API_KEY = "ENTER YOUR API KEY";
+const API_KEY = "a968776f9ba1be452d5a840698636db3";
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 // https://api.openweathermap.org/data/2.5/onecall?lat=48.8534&lon=2.3488&exclude=current,minutely,hourly,alerts&appid=1fa9ff4126d95b8db54f3897a208e91c&units=metric
